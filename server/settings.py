f2d9b5554910802d46556e9c59d47d3440e50560
emekadefirst <victorchibuogwu33@gmail.com>
2025-01-09 17:05:13 +0100

    ready

--- server/settings.py
+++ server/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_afy&yfg85b&m6r06zahwktfpv*zb54x=k_68e%@7zj452lc0r"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     "unfold.contrib.import_export",
     "import_export",
@@ -27,9 +27,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
 ]
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-]
+CORS_ALLOWED_ORIGINS = ["ol-etest.vercel.app"]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = (
     "DELETE",
